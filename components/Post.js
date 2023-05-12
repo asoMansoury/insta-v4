944639360c64116296b7af5130565f4ec66f0971
unknown <aso.mansoury@gmail.com>
2023-05-13 00:17:34 +0330

    Create the header and image sections of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,16 @@
+import {Dots} from '@heroicons/react/24/outline'
 export default function Post({caption,img,username,userImg,id}) {
   return (
-    <div>
-        <h1>{username}</h1>
+    <div className='bg-white my-7'>
+        {/**Post Header */}
+        <div className="flex items-center p-5 border rounded-md">
+            <img src={userImg} alt={username} className="h-12 rounded-full object-cover border p-1 mr-3"></img>
+            <p className='font-bold flex-1'>{username}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm7 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm7 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z"/></svg>
+        </div>
+
+        {/** */}
+        <img className='object-cover w-full' src={img}></img>
     </div>
   )
 }
